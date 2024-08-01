cdc3f7bf766f4ff0548fae8f5e5a1c00a26cce24
Elisa Silva Pacheco Monteiro <elisa.pacheco.monteiro@gmail.com>
2024-08-01 17:51:23 -0300

    Atualização

--- consulta.php
+++ consulta.php
@@ -3,6 +3,7 @@ include "conecta.php";
 $sql = "SELECT id, nome, senha, email FROM usuario";
 $resultado = mysqli_query($conexao, $sql);
 echo "<br>";
+echo "<br><b>Mostrando todos os registros<b><br>";
 if(mysqli_num_rows($resultado) > 0){
     while($registro = mysqli_fetch_assoc($resultado)){
         echo "id: " . $registro["id"]. " - Nome: " . $registro["nome"].
@@ -12,5 +13,45 @@ if(mysqli_num_rows($resultado) > 0){
 } else {
     echo "Nenhum registro encontrado.";
 }
+echo "<br><b>Nomes que começa com 'c'<b><br>";
+$sql = "SELECT id, nome, senha, email FROM usuario where nome like 'c%';";
+$resultado = mysqli_query($conexao, $sql);
+echo "<br>";
+if(mysqli_num_rows($resultado) > 0){
+    while($registro = mysqli_fetch_assoc($resultado)){
+        echo "id: " . $registro["id"]. " - Nome: " . $registro["nome"].
+        " " . $registro["senha"]. " - Email: " . $registro["email"].
+        "<br>";
+    }
+} else {
+    echo "Nenhum registro encontrado.";
+}
+echo "<br><b>Ordenar registros por email<b><br>";
+$sql = "SELECT id, nome, senha, email FROM usuario order by email;";
+$resultado = mysqli_query($conexao, $sql);
+echo "<br>";
+if(mysqli_num_rows($resultado) > 0){
+    while($registro = mysqli_fetch_assoc($resultado)){
+        echo "id: " . $registro["id"]. " - Nome: " . $registro["nome"].
+        " " . $registro["senha"]. " - Email: " . $registro["email"].
+        "<br>";
+    }
+} else {
+    echo "Nenhum registro encontrado.";
+}
+echo "<br><b>Verificar se existe usuario'Carlos' e senha='Carluxo e mostrar o seu email. Caso não existir, mostra uma mensagem '<b><br>";
+$sql = "SELECT id, nome, senha, email FROM usuario where nome = 'Carlos' and senha = 'Carluxo';";
+$resultado = mysqli_query($conexao, $sql);
+echo "<br>";
+if(mysqli_num_rows($resultado) > 0){
+    while($registro = mysqli_fetch_assoc($resultado)){
+        echo "id: " . $registro["id"]. " - Nome: " . $registro["nome"].
+        " " . $registro["senha"]. " - Email: " . $registro["email"].
+        "<br>";
+    }
+} else {
+    echo "Nenhum registro encontrado.";
+}
+
 
 ?>
